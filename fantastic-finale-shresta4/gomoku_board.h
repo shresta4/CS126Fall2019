8682de4331488aa0f86bcb92255da4f55ca4d1cc
Shresta <shrestab19@gmail.com>
2019-11-15 14:47:13 -0600

    Rearranging files

---
+++ fantastic-finale-shresta4/gomoku_board.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+using namespace std;
+constexpr int BOARD_SIZE = 19;
+constexpr int GOAL = 5;
+
+class GomokuBoard {
+    string board;
+    vector<vector<int>> lookup;
+
+    GomokuBoard();                            // empty board
+    GomokuBoard(string json_file);            // resume game stored in json file
+    void WriteBoardToJson(string json_file);  // saves game
+    vector<vector<int>> GenerateLookupTable();  // creates table of all possible
+                                                // winning combinations
+    bool PlacePiece(
+        int loc,
+        int player_id);  // places piece, B = X, W = O, false iff invalid move
+    friend ostream &operator<<(ostream &output,
+                               const GomokuBoard &gb);  // display
+};
