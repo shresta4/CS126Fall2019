23d409b0806a32a703951de79236a65de911fec0
Shresta <shrestab19@gmail.com>
2019-11-15 22:43:01 -0600

    Generate Lookup tables method

--- fantastic-finale-shresta4/src/gomoku_board.cpp
+++ fantastic-finale-shresta4/src/gomoku_board.cpp
@@ -55,11 +55,92 @@ void GomokuBoard::GenerateLookupTable() {
         int col_end = i + (BOARD_SIZE - 1) * BOARD_SIZE;
 
         // make every possible '5 in a column' within that column
-        for (int j) }
+        for (int j = col_start; j <= col_end - GOAL * BOARD_SIZE + BOARD_SIZE;
+             j += BOARD_SIZE) {
+            vector<int> sequence = vector<int>();
+            for (int k = j; k < j + GOAL * BOARD_SIZE; k += BOARD_SIZE) {
+                sequence.push_back(k);
+            }
+            lookup.push_back(sequence);
+            sequence = vector<int>();
+        }
+    }
+    // diagonals top left to bottom right, upper half
+    for (int i = 0; i < BOARD_SIZE; i++) {
+        int diagonal_start = i;
+        int diagonal_end = i + (BOARD_SIZE - 1 - i) * (BOARD_SIZE + 1);
 
-    // diagonals top left to bottom right
+        // make every possible '5 in a diagonal' within that diagonal
+        for (int j = diagonal_start;
+             j <= diagonal_end - GOAL * BOARD_SIZE + (BOARD_SIZE + 1);
+             j += (BOARD_SIZE + 1)) {
+            vector<int> sequence = vector<int>();
+            for (int k = j; k < j + GOAL * (BOARD_SIZE + 1);
+                 k += (BOARD_SIZE + 1)) {
+                sequence.push_back(k);
+            }
+            lookup.push_back(sequence);
+            sequence = vector<int>();
+        }
+    }
 
-    // diagonals top right to bottom left
+    // diagonals top left to bottom right, lower half
+    for (int i = 1; i < BOARD_SIZE;
+         i++) {  // start at 19 and go down the first column
+        int diagonal_start = i * BOARD_SIZE;
+        int diagonal_end = BOARD_SIZE * BOARD_SIZE - (i + 1);
+
+        // make every possible '5 in a diagonal' within that diagonal
+        for (int j = diagonal_start;
+             j <= diagonal_end - GOAL * BOARD_SIZE + (BOARD_SIZE + 1);
+             j += (BOARD_SIZE + 1)) {
+            vector<int> sequence = vector<int>();
+            for (int k = j; k < j + GOAL * (BOARD_SIZE + 1);
+                 k += (BOARD_SIZE + 1)) {
+                sequence.push_back(k);
+            }
+            lookup.push_back(sequence);
+            sequence = vector<int>();
+        }
+    }
+
+    // diagonals top right to bottom left, top half
+    for (int i = 0; i < BOARD_SIZE; i++) {
+        int diagonal_start = i;
+        int diagonal_end = i * BOARD_SIZE;
+
+        // make every possible '5 in a diagonal' within that diagonal
+        for (int j = diagonal_start;
+             j <= diagonal_end - (GOAL - 1) * (BOARD_SIZE - 1);
+             j += (BOARD_SIZE - 1)) {
+            vector<int> sequence = vector<int>();
+            for (int k = j; k < j + (GOAL) * (BOARD_SIZE - 1);
+                 k += (BOARD_SIZE - 1)) {
+                sequence.push_back(k);
+            }
+            lookup.push_back(sequence);
+            sequence = vector<int>();
+        }
+    }
+
+	// diagonals top right to bottom left, bottom half
+    for (int i = BOARD_SIZE * 2 - 1; i < BOARD_SIZE * BOARD_SIZE; i++) {
+        int diagonal_start = i;
+        int diagonal_end = (BOARD_SIZE * BOARD_SIZE - 1) - (BOARD_SIZE - (diagonal_start + 1) / BOARD_SIZE);
+
+        // make every possible '5 in a diagonal' within that diagonal
+        for (int j = diagonal_start;
+             j <= diagonal_end - (GOAL - 1) * (BOARD_SIZE - 1);
+             j += (BOARD_SIZE - 1)) {
+            vector<int> sequence = vector<int>();
+            for (int k = j; k < j + (GOAL) * (BOARD_SIZE - 1);
+                 k += (BOARD_SIZE - 1)) {
+                sequence.push_back(k);
+            }
+            lookup.push_back(sequence);
+            sequence = vector<int>();
+        }
+    }
 }
 
 bool GomokuBoard::PlacePiece(int loc, int player_id) {
